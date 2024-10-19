b13c6af46dee6f17311cb023449a60bec7ad0f80
SatriaAhmadiuz <satriaahmadiuz5@gmail.com>
2024-10-19 12:45:16 +0700

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+int nilai;
+cout<<"Masukan Sebuah Nilai : "; 
+cin>>nilai;
+if(nilai>70)
+cout<<"\nLULUS";
+else
+cout<<"\nTIDAK LULUS!!!!MAKANNYA BELAJARR!!!!";
+    return 0;
+}
