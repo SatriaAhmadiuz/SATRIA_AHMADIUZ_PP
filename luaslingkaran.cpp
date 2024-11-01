bb4af2f852ec05bf4d0eb26b0df7882cb1cc015b
SatriaAhmadiuz <satriaahmadiuz5@gmail.com>
2024-11-01 15:35:01 +0700

    Add files via upload

---
+++ luaslingkaran.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float phi,r,luas ;
+
+phi=3,14;
+cout << "Mari Menghitung Luas Lingkaran" "\n";
+cout << "Masukkan Jari Jari : ";
+cin >> r ;
+luas = phi*r*r ;
+cout << "Luas lingkaran adalah :" <<luas;
+    return 0;
+}
